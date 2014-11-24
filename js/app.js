f8c1fd34da40dfaf06b0b363bc8f6a5b0a98386e
Morya Breland Jr <morya.58@gmail.com>
2014-11-23 23:56:49 -0800

    Finished Seattle Traffic Cam Mapping project

--- js/app.js
+++ js/app.js
@@ -11,3 +11,58 @@
 //you should also write the code to filter the set of markers when the user
 //types a search phrase into the search box
 
+$(document).ready(function () {
+    var mapElem = document.getElementById('map');
+    var center = {
+        lat: 47.6,
+        lng: -122.3
+    };
+
+    var map = new google.maps.Map(mapElem, {
+        center: center,
+        zoom: 14
+    });
+
+    var infoWindow = new google.maps.InfoWindow();
+
+    $.getJSON('http://data.seattle.gov/resource/65fc-btcc.json')
+        .done(function (data) {
+            data.forEach(function (camera) {
+                var marker = new google.maps.Marker({
+                    position: {
+                        lat: Number(camera.location.latitude),
+                        lng: Number(camera.location.longitude)
+                    },
+                    map : map
+                });
+                google.maps.event.addListener(marker, 'click', function() {
+                    var html = '<img src=' + camera.imageurl.url + '>';
+                    html += '<h2>' + camera.cameralabel + '</h2>';
+                    html += '<p>' + camera.ownershipcd + '</p>';
+                    map.panTo(this.getPosition());
+                    infoWindow.setContent(html);
+                    infoWindow.open(map, this);
+                });
+
+
+                $("#search").bind('search keyup', function() {
+                    if(camera.cameralabel.toLowerCase().indexOf($("#search").val().toLowerCase()) < 0) {
+                        marker.setMap(null);
+                    } else {
+                        marker.setMap(map);
+                    }
+                });
+            });
+
+            $(window).resize(function () {
+                map.height(window.innerHeight - map.position())
+            })
+
+        })
+        .fail(function (error) {
+            window.alert('Uh Oh we had this ' + error)
+        })
+        .always(function () {
+
+        });
+});
